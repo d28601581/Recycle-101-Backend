b5497eb20f67341b2c608aac412a07861be8e5dd
Mohammad Hossain <moh.hossain98@gmail.com>
2021-01-26 17:17:14 -0500

    added all the routes

--- api/items.js
+++ api/items.js
@@ -18,13 +18,8 @@ router.get('/', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     const item = await Item.create(
-
-      req.body
-      // {
-      //   itemName: req.body.itemName, quantity: req.body.quantity, category: req.body.category, points: req.body.points
-      // }
+      req.query
     );
-    
   } catch (error) {
     next(error);
   }
@@ -32,35 +27,32 @@ router.post('/', async (req, res, next) => {
 
 router.put('/:id', async (req, res, next) => {
   try {
-    const item = Item.findByPk(req.params.id);
+    // const item = Item.findByPk(req.params.id);
 
-    await item.update(
-      {
-        itemName: req.body.itemName, quantity: req.body.quantity, category: req.body.category, points: req.body.points
-      }
+    await Item.update(
+      
+        {itemName: req.query.itemName,
+        quantity: req.query.quantity,
+        category: req.query.category,
+        points: req.query.points
+        }, {where:{id: req.params.id}}, {multi: true} 
+      
     )
-    item.save();
     // An if/ternary statement to handle not finding allPlayers explicitly
   } catch (error) {
     next(error);
   }
 });
 
-router.delete('/', async (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
   try {
-    const allItems = await Item.findAll();
-    // An if/ternary statement to handle not finding allPlayers explicitly
-    !allItems
-      ? res.status(404).send('Players Listing Not Found')
-      : res.status(200).json(allItems);
+    await Item.destroy({where:{id: req.params.id}}, {multi: true} 
+    
+  )
   } catch (error) {
     next(error);
   }
 });
-
-
-
-
 
 // Export our router, so that it can be imported to construct our api routes
 module.exports = router;
--- db/models/userData.js
+++ db/models/userData.js
@@ -4,8 +4,7 @@ const db = require('../db');
 //Sample Model  Read More At https://sequelize.org/master/manual/model-basics.html
 
 const UserData = db.define('userData', {
-
-
+  
   total:{
     type: Sequelize.INTEGER,
     allowNull: false
