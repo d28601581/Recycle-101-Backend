a4da1675b4b6010d385895b913975683870e943a
Mohammad Hossain <moh.hossain98@gmail.com>
2021-01-26 01:43:41 -0500

    Mohammad 1: seeded, synced database

--- db/db.js
+++ db/db.js
@@ -1,12 +1,11 @@
 const Sequelize = require('sequelize');
 const { name } = require('../package.json');
+require("dotenv").config();
 
 // Initialize database with Sequelize
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
-  {
-    logging: false,
-  }
-);
+const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
 
 module.exports = db;
--- seed.js
+++ seed.js
@@ -1,13 +1,34 @@
 const db = require('./db');
 const Players = require('./db/models').Player;
 
-const seedPlayers = [
-  { firstName: 'Bilbo', lastName: 'Baggins', jerseyNumber: 11 },
-  { firstName: 'Harry', lastName: 'Potter', jerseyNumber: 22 },
-  { firstName: 'Lucifer', lastName: 'Morningstart', jerseyNumber: 666 },
-];
+// const seedPlayers = [
+//   { firstName: 'Bilbo', lastName: 'Baggins', jerseyNumber: 11 },
+//   { firstName: 'Harry', lastName: 'Potter', jerseyNumber: 22 },
+//   { firstName: 'Lucifer', lastName: 'Morningstart', jerseyNumber: 666 },
+// ];
 
-const seed = () => {
-  return Players.bulkCreate(seedPlayers);
-};
+// const seed = () => {
+//   return Players.bulkCreate(seedPlayers);
+// };
+
+const seed = async () => {
+  console.log('before')
+  await Promise.all([
+    
+    Players.create({
+      firstName: 'Bilbo', lastName: 'Baggins', jerseyNumber: 11
+    }),
+    Players.create({
+      firstName: 'Harry', lastName: 'Potter', jerseyNumber: 22
+    }),
+    Players.create({
+      firstName: 'Lucifer', lastName: 'Morningstart', jerseyNumber: 666
+    }),
+  ]);
+  console.log('after')
+}
+
+console.log('seeding')
 seed().then(() => process.exit());
+
+module.exports = seed;
