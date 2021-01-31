2d954be2dce6fed9043eb5800f05c99f0a1d80ed
Ariel Benzur <aab171@outlook.com>
2021-01-31 14:53:46 -0500

    Edited db.js

--- db/db.js
+++ db/db.js
@@ -3,12 +3,17 @@ const { name } = require('../package.json');
 require("dotenv").config();
 
 // Initialize database with Sequelize
-// DB_NAME, added porotocol & dialectOptions, 
-const db = new Sequelize(`${process.env.HEROKU_POSTGRESQL_PUCE_URL}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+/* const db = new Sequelize(`${process.env.NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  logging: true
+  }
+});
+ */
+
+const db = new Sequelize(`${process.env.HEROKU_POSTGRESQL_PUCE_URL}`, {
+  dialect: 'postgres',
   protocol: 'postgres',
-  logging: true,
   dialectOptions: {
     ssl: {
       sslmode: 'require',
